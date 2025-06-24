1c5ff915fc23f1bb6eec5e72df57e249758a1b1a
Yasin <Yasin@DESKTOP-LBNF79E>
2025-06-24 16:09:33 +0300

    add login page

--- ProductBlazorApp1/Program.cs
+++ ProductBlazorApp1/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ProductBlazorApp1.Data;
@@ -6,20 +7,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredLocalStorage();
+//builder.Services.AddBlazoredLocalStorage(config =>
+//{
+//    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+//    config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+//    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+//    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+//    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+//    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+//    config.JsonSerializerOptions.WriteIndented = false;
+//});
 
-// Veritabaný baðlantýsý
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=productreviewNew.db"));
 
-// Identity servisi
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
 })
 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+
 var app = builder.Build();
 
+// Kullanýcý ekleme iþlemi - async çalýþtýrýlmasý gereken blok
 using (var scope = app.Services.CreateScope())
 {
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
@@ -35,7 +47,7 @@ using (var scope = app.Services.CreateScope())
             EmailConfirmed = true
         };
 
-        var result = await userManager.CreateAsync(newUser, "User123!"); //þifre
+        var result = await userManager.CreateAsync(newUser, "User123!");
         if (result.Succeeded)
         {
             Console.WriteLine("Seed kullanýcý baþarýyla eklendi.");
@@ -48,6 +60,36 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+// admin
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+    var adminEmail = "admin@gmail.com";
+    var adminUser = await userManager.FindByEmailAsync(adminEmail);
+    if (adminUser == null)
+    {
+        var newAdmin = new ApplicationUser
+        {
+            UserName = adminEmail,
+            Email = adminEmail,
+            EmailConfirmed = true
+        };
+
+        var result = await userManager.CreateAsync(newAdmin, "Admin123!");
+        if (result.Succeeded)
+        {
+            Console.WriteLine("Admin kullanýcý baþarýyla eklendi.");
+        }
+        else
+        {
+            foreach (var error in result.Errors)
+                Console.WriteLine($"Hata: {error.Description}");
+        }
+    }
+}
+
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -58,7 +100,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-app.UseAuthentication(); // Giriþ iþlemleri için gerekli
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapBlazorHub();
